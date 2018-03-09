ccb74d4ab8a122a0af46d68abd488af9df62431f
krazykieka <jessicaleehammond@gmail.com>
2018-03-08 21:07:14 -0800

    update work and file structure

---
+++ python_challenge/PyBank/main.py
@@ -0,0 +1,55 @@
+import csv
+import os
+import pandas
+import numpy
+
+
+csvpath = os.path.join('raw_data', 'budget_data_1.csv')
+columnsum=numpy.column2('raw_data', 'budget_data_1.csv')
+
+print('Financial Analysis')
+print('_________________________')
+with open(csvpath, newline='') as f:
+    next(f)
+    counter=csv.reader(f,delimiter=",")
+    
+    totalmonths = str(len(list(counter)))
+    print( "Total Months: " + totalmonths)
+    f.seek(0)
+    
+    next(f)
+    total = 0
+    for row in counter:
+        total+=int(row[1])
+    money=float(total)
+    amountindollars = '${:,.2f}'.format(money)
+    print("Total revenue: " +amountindollars)
+    f.seek(0)
+    
+    next(f)
+    print(columnsum[].sum())
+    print("Average Revenue : " + )
+#    max = 0
+#    maxdate = "date"
+#    for row in counter:
+#        if int(row[1])>int(max):
+#                max = row[1]
+#                maxdate=row[0]
+#    money2=float(max) 
+#    amountindollars2 = '${:,.2f}'.format(money2)
+#    print("Average Revenue: " + float(int(amountindollars2/totalmonths)))
+#    f.seek(0)
+#    
+#    next(f)
+#
+#title = input("Which title are you looking for?")
+#is_found = tFalse
+#
+#with open(csvpath, 'rt') as f:
+#    for row in reader:
+#        if title == row[0]:
+#            print("You are in luck! We have that title!" + row[0] + " is rated " + row[1] + " with a rating of " + row[6])
+#            is_found = True
+#            break
+#    if is_found == False:
+#        print("I am sorry we do not have that title.")
---
+++ python_challenge/PyBoss/main.py
@@ -0,0 +1,5 @@
+import os
+import csv
+
+emp1 = os.path.join('raw_data', 'employee_data1.csv')
+emp2 = os.path.join('raw_data', 'employee_data2.csv')
