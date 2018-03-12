ed25d4e65d0450d48519c499acc08210e60fe7ca
krazykieka <jessicaleehammond@gmail.com>
2018-03-12 16:35:57 -0700

    up

--- python_challenge/PyPara/main.py
+++ python_challenge/PyPara/main.py
@@ -1,1 +1,20 @@
 import re
+import os
+from collections import Counter
+import string
+
+para = os.path.join('para.txt')
+
+words = 0
+parawords = 0
+
+with open(para, 'r' ) as f:
+    for line in f:
+        #Split Paragraph on basis of '.' or ? or !.
+        sen = re.split(r"\.|\?|\!", line)
+        len(sen)
+        print(sen)
+        #Split line into list using space.
+        words = sen.split(" ")
+        numwords = len(words)
+        print(numwords)
